594a337a44ef9f0c4567cf230ec869c5ec01e40a
Vladimir Konnov <vladimir.konnov@gmail.com>
2019-04-04 14:48:37 +0400

    Fixed calculations with last day of month

--- app/controllers/payrolls_controller.rb
+++ app/controllers/payrolls_controller.rb
@@ -9,7 +9,8 @@ class PayrollsController < ApplicationController
     @date = Date.new(year, month, 1).beginning_of_month
     @months = Lesson.select(:date).group(:date).order(:date).map(&:date).map(&:beginning_of_month).uniq{|d| [d.month, d.year]}
     @payroll = PayrollsService.new(current_user, @date).payroll
-    @subscriptions = Subscription.where(user_id: @payroll.keys.map(&:id), purchase_date: @date..@date.end_of_month)
+    @subscriptions = Subscription.where(user_id: @payroll.keys.map(&:id))
+                       .where('purchase_date > ? and purchase_date <= ?', @date, @date.end_of_month)
                        .order(:purchase_date).preload(:student)#.preload(:creator)
     @subscriptions = @subscriptions.group_by(&:creator)
   end
