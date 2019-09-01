4de684d0852f628ac70a92bad85e0a3b7c702099
Vladimir Konnov <vladimir.konnov@gmail.com>
2019-09-01 17:18:11 +0400

    Final touch for students sorting

--- app/services/student_visits_service.rb
+++ app/services/student_visits_service.rb
@@ -4,7 +4,7 @@ class StudentVisitsService
     LessonStudent.joins(:lesson).where('lessons.date between ? AND ?', from, to).
         where('subscription_id is not null').group('lessons_students.student_id').
         select('lessons_students.student_id, count(lessons_students.student_id) as visits').
-        order('count(lessons_students.student_id) DESC').each do |row|
+        order('count(lessons_students.student_id) DESC, lessons_students.student_id').each do |row|
       result[row.student_id] = row.visits
     end
     students = Hash[Student.where(id: result.keys).map { |s| [s.id, s] }]
