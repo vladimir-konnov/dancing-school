f1898b0646d7b70d8656a3fc3359e713857a1875
Vladimir Konnov <vladimir.konnov@gmail.com>
2022-11-11 16:47:13 +0400

    Initialized active storage

--- db/schema.rb
+++ db/schema.rb
@@ -2,18 +2,45 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# This file is the source Rails uses to define your schema when running `rails
-# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
 # be faster and is potentially less error prone than running all of your
 # migrations from scratch. Old migrations may fail to apply correctly if those
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_21_102243) do
-
+ActiveRecord::Schema[7.0].define(version: 2022_11_11_124242) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.string "service_name", null: false
+    t.bigint "byte_size", null: false
+    t.string "checksum"
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "active_storage_variant_records", force: :cascade do |t|
+    t.bigint "blob_id", null: false
+    t.string "variation_digest", null: false
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
 
   create_table "lessons", force: :cascade do |t|
     t.date "date"
@@ -24,8 +51,8 @@ ActiveRecord::Schema.define(version: 2020_09_21_102243) do
     t.bigint "lesson_id", null: false
     t.bigint "student_id", null: false
     t.bigint "subscription_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.index ["lesson_id", "student_id", "subscription_id"], name: "index_lessons_students_subscriptions"
     t.index ["lesson_id", "student_id"], name: "index_lessons_students_on_lesson_id_and_student_id", unique: true
     t.index ["lesson_id"], name: "index_lessons_students_on_lesson_id"
@@ -36,8 +63,8 @@ ActiveRecord::Schema.define(version: 2020_09_21_102243) do
   create_table "lessons_users", id: false, force: :cascade do |t|
     t.bigint "lesson_id", null: false
     t.bigint "user_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.index ["lesson_id", "user_id"], name: "index_lessons_users_on_lesson_id_and_user_id", unique: true
     t.index ["lesson_id"], name: "index_lessons_users_on_lesson_id"
     t.index ["user_id"], name: "index_lessons_users_on_user_id"
@@ -45,8 +72,8 @@ ActiveRecord::Schema.define(version: 2020_09_21_102243) do
 
   create_table "roles", force: :cascade do |t|
     t.string "name", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
   end
 
   create_table "students", force: :cascade do |t|
@@ -57,16 +84,16 @@ ActiveRecord::Schema.define(version: 2020_09_21_102243) do
     t.string "email"
     t.string "vk_profile"
     t.text "comment"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.date "birthday"
   end
 
   create_table "styles", force: :cascade do |t|
     t.string "name", null: false
     t.decimal "duration_hours", precision: 4, scale: 2, default: "1.0", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.boolean "calculate_payrolls", default: true, null: false
     t.boolean "party_subscription", default: false, null: false
     t.boolean "visible", default: true, null: false
@@ -85,8 +112,8 @@ ActiveRecord::Schema.define(version: 2020_09_21_102243) do
     t.string "name", null: false
     t.integer "number_of_lessons", default: 1, null: false
     t.decimal "cost", precision: 8, scale: 2, default: "300.0", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.integer "duration_months", default: 0, null: false
     t.integer "duration_weeks", default: 4, null: false
     t.boolean "active", default: true, null: false
@@ -102,8 +129,8 @@ ActiveRecord::Schema.define(version: 2020_09_21_102243) do
     t.date "expiry_date"
     t.decimal "price", default: "0.0", null: false
     t.bigint "paired_with_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.bigint "user_id", null: false
     t.boolean "no_expiry", default: false, null: false
     t.integer "number_of_lessons", default: 0, null: false
@@ -118,22 +145,22 @@ ActiveRecord::Schema.define(version: 2020_09_21_102243) do
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
+    t.datetime "reset_password_sent_at", precision: nil
+    t.datetime "remember_created_at", precision: nil
     t.integer "sign_in_count", default: 0, null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
+    t.datetime "current_sign_in_at", precision: nil
+    t.datetime "last_sign_in_at", precision: nil
     t.inet "current_sign_in_ip"
     t.inet "last_sign_in_ip"
     t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
+    t.datetime "confirmed_at", precision: nil
+    t.datetime "confirmation_sent_at", precision: nil
     t.string "unconfirmed_email"
     t.integer "failed_attempts", default: 0, null: false
     t.string "unlock_token"
-    t.datetime "locked_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "locked_at", precision: nil
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.string "firstname", null: false
     t.string "lastname", null: false
     t.string "middlename"
@@ -148,13 +175,15 @@ ActiveRecord::Schema.define(version: 2020_09_21_102243) do
   create_table "users_roles", id: false, force: :cascade do |t|
     t.bigint "user_id", null: false
     t.bigint "role_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.index ["role_id"], name: "index_users_roles_on_role_id"
     t.index ["user_id", "role_id"], name: "index_users_roles_on_user_id_and_role_id", unique: true
     t.index ["user_id"], name: "index_users_roles_on_user_id"
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "lessons", "styles"
   add_foreign_key "lessons_students", "lessons"
   add_foreign_key "lessons_students", "students"
